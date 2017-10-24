5fe7133c8e3662775150218af2c1d7c7a6d463ae
Marcin Wisniowski <hollowlink@gmail.com>
2017-10-24 00:01:14 -0400

    Create Import-ADUser.ps1

---
+++ Import-ADUser.ps1
@@ -0,0 +1,38 @@
+Function Import-ADUser
+{
+  param
+  (
+      $userListFile = "UserList.csv"
+  )
+
+  $users = Import-Csv $userListFile
+
+  foreach ($u in $users)
+  {
+      $user = @{ 
+          "Name" = [string]"$($u.Name)";
+          "DisplayName" = [string]"$($u.Name)";
+          "GivenName" = [string]"$($u.GivenName)";
+          "Surname" = [string]"$($u.Surname)";
+          "SamAccountName" = [string]"$($u.SamAccountName)";
+          "Description" = [string]"$($u.Description)";
+          "EmailAddress" = [string]"$($u.EmailAddress)";
+          "UserPrincipalName" = [string]("$($u.SamAccountName)@$((Get-ADDomain).DNSRoot)");
+          "Path" = [string]"$($u.Path)";
+          "AccountPassword" = ConvertTo-SecureString "$($u.AccountPassword)" -AsPlainText -Force;
+          "ChangePasswordAtLogon" = $false;
+          "Enabled" = $true;
+          "PasswordNeverExpires" = $true
+      }
+
+      try
+      {
+          $user = Get-ADUser $user.SamAccountName -ErrorAction SilentlyContinue
+          Write-Error "-----> IMPORT Error: User $($user.SamAccountName) already exists."
+      }
+      catch
+      {
+          $user = New-ADUser @user
+      }
+  }
+}
