ff401686ff4f0dfd30327cf0dbdf66174559f241
Marcin Wisniowski <hollowlink@gmail.com>
2017-10-24 00:40:06 -0400

    Update Import-ADUser.ps1

--- Import-ADUser.ps1
+++ Import-ADUser.ps1
@@ -1,13 +1,42 @@
+# Licensed to the Apache Software Foundation (ASF) under one
+# or more contributor license agreements.  See the NOTICE file
+# distributed with this work for additional information
+# regarding copyright ownership.  The ASF licenses this file
+# to you under the Apache License, Version 2.0 (the
+# "License"); you may not use this file except in compliance
+# with the License.  You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing,
+# software distributed under the License is distributed on an
+# "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+# KIND, either express or implied.  See the License for the
+# specific language governing permissions and limitations
+# under the License.
+
 Function Import-ADUser
 {
+  <#
+  .Synopsis
+  Creates new Active Directory users in batch.
+  .Description
+  Creates new Active Directory users in batch, reading account information from
+  a csv file and passing to New-ADUser.
+  #>
   param
   (
-      $userListFile = "UserList.csv"
+    [Parameter(
+      Position=0,
+      ValueFromPipeline=$true,
+      ValueFromPipelineByPropertyName=$true)
+    ]
+    [string]$Path = "UserList.csv"
   )
 
-  $users = Import-Csv $userListFile
+  $Users = Import-Csv $Path
 
-  foreach ($u in $users)
+  foreach ($u in $Users)
   {
     $user = @{ 
       "Name" = [string]"$($u.Name)";
