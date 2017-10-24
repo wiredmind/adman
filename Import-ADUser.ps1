863ada39744a30a0cc9f275e8a092c1bb3e604a7
Marcin Wisniowski <hollowlink@gmail.com>
2017-10-24 00:10:06 -0400

    Update Import-ADUser.ps1

--- Import-ADUser.ps1
+++ Import-ADUser.ps1
@@ -9,30 +9,30 @@ Function Import-ADUser
 
   foreach ($u in $users)
   {
-      $user = @{ 
-          "Name" = [string]"$($u.Name)";
-          "DisplayName" = [string]"$($u.Name)";
-          "GivenName" = [string]"$($u.GivenName)";
-          "Surname" = [string]"$($u.Surname)";
-          "SamAccountName" = [string]"$($u.SamAccountName)";
-          "Description" = [string]"$($u.Description)";
-          "EmailAddress" = [string]"$($u.EmailAddress)";
-          "UserPrincipalName" = [string]("$($u.SamAccountName)@$((Get-ADDomain).DNSRoot)");
-          "Path" = [string]"$($u.Path)";
-          "AccountPassword" = ConvertTo-SecureString "$($u.AccountPassword)" -AsPlainText -Force;
-          "ChangePasswordAtLogon" = $false;
-          "Enabled" = $true;
-          "PasswordNeverExpires" = $true
-      }
+    $user = @{ 
+      "Name" = [string]"$($u.Name)";
+      "DisplayName" = [string]"$($u.Name)";
+      "GivenName" = [string]"$($u.GivenName)";
+      "Surname" = [string]"$($u.Surname)";
+      "SamAccountName" = [string]"$($u.SamAccountName)";
+      "Description" = [string]"$($u.Description)";
+      "EmailAddress" = [string]"$($u.EmailAddress)";
+      "UserPrincipalName" = [string]("$($u.SamAccountName)@$((Get-ADDomain).DNSRoot)");
+      "Path" = [string]"$($u.Path)";
+      "AccountPassword" = ConvertTo-SecureString "$($u.AccountPassword)" -AsPlainText -Force;
+      "ChangePasswordAtLogon" = $false;
+      "Enabled" = $true;
+      "PasswordNeverExpires" = $true
+    }
 
-      try
-      {
-          $user = Get-ADUser $user.SamAccountName -ErrorAction SilentlyContinue
-          Write-Error "-----> IMPORT Error: User $($user.SamAccountName) already exists."
-      }
-      catch
-      {
-          $user = New-ADUser @user
-      }
+    try
+    {
+      $user = Get-ADUser $user.SamAccountName -ErrorAction SilentlyContinue
+      Write-Error "-----> IMPORT ERROR: User $($user.SamAccountName) already exists." -Category InvalidArgument
+    }
+    catch
+    {
+      $user = New-ADUser @user
+    }
   }
 }
